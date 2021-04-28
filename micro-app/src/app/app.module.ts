ceb3270818d857e1663837dfdc8b6e5943adee9c
Roman Kohutyak <romankohutyak@gmail.com>
2021-04-28 18:31:47 +0300

    refactor: create separate header component

--- micro-app/src/app/app.module.ts
+++ micro-app/src/app/app.module.ts
@@ -7,9 +7,10 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HomeComponent } from './home/home.component';
 import { InfoComponent } from './info/info.component';
 import { ProjectsComponent } from './projects/projects.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, InfoComponent, ProjectsComponent],
+  declarations: [AppComponent, HomeComponent, InfoComponent, ProjectsComponent, HeaderComponent],
   imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
