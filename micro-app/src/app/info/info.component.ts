e6aa7135c6739107f623c0143a7b6f45c0e49ec4
Roman Kohutyak <romankohutyak@gmail.com>
2021-04-28 23:21:03 +0300

    refactor: learning string interpolation

--- micro-app/src/app/info/info.component.ts
+++ micro-app/src/app/info/info.component.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
 
+type Course = {
+  name: string;
+  startDate: number;
+  endDate: number;
+  link?: string;
+};
+
+type Courses = Array<Course>;
+
 @Component({
   selector: 'app-info',
   templateUrl: './info.component.html',
-  styleUrls: ['./info.component.scss']
+  styleUrls: ['./info.component.scss'],
 })
 export class InfoComponent implements OnInit {
+  rs: Courses = [
+    {
+      name: 'The Rolling Scopes: JS Front End Course.',
+      startDate: 2020.09,
+      endDate: 2021.02,
+      link:
+        'https://rrroman.github.io/certificates/images/certificate-rs-front-end-2020-2021.png',
+    },
+    {
+      name: 'React Course. Link incoming.',
+      startDate: 2021.02,
+      endDate: 2021.04,
+    },
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
