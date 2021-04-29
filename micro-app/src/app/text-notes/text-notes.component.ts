68f22257c41ab26b19a858c7fe452bcdcb304162
Roman Kohutyak <romankohutyak@gmail.com>
2021-04-29 23:10:19 +0300

    feat: colorized typing status

--- micro-app/src/app/text-notes/text-notes.component.ts
+++ micro-app/src/app/text-notes/text-notes.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TextNotesComponent implements OnInit {
   note: string = '';
+  isTyping: boolean = false;
+  color: string;
 
   constructor() {}
 
@@ -14,9 +16,25 @@ export class TextNotesComponent implements OnInit {
 
   onTypingNote(event: Event): void {
     this.note = (<HTMLInputElement>event.target).value;
+    this.colorize();
+
+    if (this.isTyping) {
+      return;
+    }
+
+    this.isTyping = true;
+    setTimeout(() => {
+      this.isTyping = false;
+    }, 2000);
   }
 
   addNote() {
     console.log('Submitting', this.note);
+    this.note = '';
+  }
+
+  colorize() {
+    console.log(`#${Math.floor(Math.random() * 16777215).toString(16)}`);
+    this.color = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
   }
 }
