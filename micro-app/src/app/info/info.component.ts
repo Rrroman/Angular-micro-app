c7ebab99e4052cbc511e64b07a8ed908a2eff191
Roman Kohutyak <romankohutyak@gmail.com>
2021-04-30 09:16:06 +0300

    feat: add skills

--- micro-app/src/app/info/info.component.ts
+++ micro-app/src/app/info/info.component.ts
@@ -9,6 +9,12 @@ type Course = {
 
 type Courses = Array<Course>;
 
+type skill = {
+  name: string;
+  description: string;
+};
+type skills = Array<skill>;
+
 @Component({
   selector: 'app-info',
   templateUrl: './info.component.html',
@@ -27,6 +33,44 @@ export class InfoComponent implements OnInit {
       name: 'React Course. Link in progress.',
       startDate: 2021.02,
       endDate: 2021.04,
+    },
+  ];
+
+  skills: skills = [
+    {
+      name: 'React',
+      description:
+        ' - an open-source, front end, JavaScript library for building user interfaces or UI components.',
+    },
+    {
+      name: 'Angular',
+      description:
+        ' - a TypeScript-based open-source web application framework led by the Angular Team at Google and by a community of individuals and corporations.',
+    },
+    {
+      name: 'TypeScript',
+      description:
+        ' - an open-source language which builds on JavaScript, one of the world’s most used tools, by adding static type definitions.',
+    },
+    {
+      name: 'JavaScript',
+      description:
+        ' - a lightweight, interpreted, or just-in-time compiled programming language, prototype-based, multi-paradigm, single-threaded, dynamic language, supporting object-oriented, imperative, and declarative (e.g. functional programming) styles.',
+    },
+    {
+      name: 'CSS',
+      description:
+        ' - a style sheet language used for describing the presentation of a document written in a markup language such as HTML.',
+    },
+    {
+      name: 'HTML',
+      description:
+        ' - the standard markup language for documents designed to be displayed in a web browser.',
+    },
+    {
+      name: 'GIT',
+      description:
+        ' - a free and open source distributed version control system designed to handle everything from small to very large projects with speed and efficiency.',
     },
   ];
 
